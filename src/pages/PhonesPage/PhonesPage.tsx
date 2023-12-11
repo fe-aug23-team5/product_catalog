42efe68ce5cecaf5a3cee0bbe4fed52b4206003a
Yuliia Bakun <bakunyuliia@gmail.com>
2023-12-11 14:32:47 +0100

    add pages layer

---
+++ src/pages/PhonesPage/PhonesPage.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const PhonesPage: React.FC = () => {
+  return (
+    <h1>PhonesPage</h1>
+  );
+};
---
+++ src/pages/ProductDetailsPage/ProductDetailsPage.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const ProductDetailsPage: React.FC = () => {
+  return (
+    <h1>ProductDetailsPage</h1>
+  );
+};
