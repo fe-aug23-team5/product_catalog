0d679f71bc814079c9ce8d5026f21f9a9fca9707
Yevhenii Polishchuk <zenapolisuk26@gmail.com>
2023-12-11 20:45:09 +0200

    added header with adaptive layout second attempt

--- src/widgets/Header/Header.tsx
+++ src/widgets/Header/Header.tsx
@@ -1,7 +1,31 @@
 import React from 'react';
+import styles from './Header.module.scss';
+import { ReactComponent as Logo } from '../../img/icons/Logo.svg';
+import { ReactComponent as Favorites } from '../../img/icons/Favourites (Heart Like).svg'
+import { ReactComponent as Cart } from '../../img/icons/Shopping bag (Cart).svg';
 
 export const Header: React.FC = () => {
   return (
-    <h1>Header</h1>
+    <header className={styles.header}>
+      <div className={styles.header__leftSide}>
+        <a href="#" className={styles.header__logo}>
+          <Logo />
+        </a>
+        <nav className={styles.nav}>
+          <a href="#" className={styles.nav__item__active}>Home</a>
+          <a href="#" className={styles.nav__item}>Phones</a>
+          <a href="#" className={styles.nav__item}>Tablets</a>
+          <a href="#" className={styles.nav__item}>Accessories</a>
+        </nav>
+      </div>
+      <div className={styles.header__rightSide}>
+        <a href="#" className={styles.icon__item}>
+          <Favorites />
+        </a>
+        <a href="#" className={styles.icon__item}>
+          <Cart />
+        </a>
+      </div>
+    </header>
   );
 };
