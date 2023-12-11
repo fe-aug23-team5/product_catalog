8ca9bd8de08dc23e487dc3444f7293d8886aab2e
Dmytro <tilugdmytro@gmail.com>
2023-12-11 21:26:46 +0200

    lint-staged test

--- src/code.ts
+++ src/code.ts
@@ -3,9 +3,9 @@ const num2 = 8;
 
 function check(value) {
   if (value == undefined) {
-    console.log("first");
+    console.log('first');
   }
 }
 
 console.log(num1 == num2);
-console.log("hello");
+console.log('hello');
