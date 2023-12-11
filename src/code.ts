e44e8cc703e6cbe29f929c8963a26018085c7be7
Dmytro <tilugdmytro@gmail.com>
2023-12-11 21:56:04 +0200

    lint-staged test

---
+++ src/code.ts
@@ -0,0 +1,14 @@
+// const num1 = 5;
+// const num2 = 8;
+// const num5 = 8;
+
+function check(value) {
+  if (value === undefined) {
+    throw new Error('kjjkj');
+  }
+}
+
+check(5);
+
+// console.log(num1 == num2);
+// console.log('hello');
