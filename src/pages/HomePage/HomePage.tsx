d764f030093e15606464068c92a28cf5cbfae958
Yuliia Bakun <bakunyuliia@gmail.com>
2023-12-12 16:03:02 +0100

    home page structure

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,5 +1,33 @@
 import React from 'react';
+import './HomePage.scss';
+import { ProductCard } from '../../entities/ProductCard';
+import { ProductSlider } from '../../features/ProductSlider';
+import { CategoriesMenu } from '../../widgets/CategoriesMenu';
+import { PhotosSlider } from '../../features/PhotosSlider';
+import { PageTitle } from '../../shared/ui/PageTitle';
 
 export const HomePage: React.FC = () => {
-  return <h1>HomePage</h1>;
+  return (
+    <div className="content_container">
+      <div className="content_container_item">
+        <PageTitle>Welcome to Nice Gadgets Store</PageTitle>
+
+        <PhotosSlider />
+      </div>
+
+      <div className="content_container_item">
+        <ProductSlider />
+      </div>
+
+      <div className="content_container_item">
+        <CategoriesMenu />
+      </div>
+
+      <div className="content_container_item">
+        <ProductSlider />
+      </div>
+
+      <ProductCard />
+    </div>
+  );
 };
