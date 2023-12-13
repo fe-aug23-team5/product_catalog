c07eee9eaba8f59b4be05d8c097cc25eb1118edf
Yuliia Bakun <bakunyuliia@gmail.com>
2023-12-13 08:44:05 +0100

    general fixes and improvements

--- src/entities/ProductCard/ProductCard.tsx
+++ src/entities/ProductCard/ProductCard.tsx
@@ -3,46 +3,65 @@ import classes from './ProductCard.module.scss';
 import img from '../../img/phones/apple-iphone-7/silver/00.jpg';
 import { PrimaryButton } from '../../shared/ui/PrimaryButton';
 import { IconButton } from '../../shared/ui/IconButton';
+import { Phone } from '../../shared/types/Phone';
 
-export const ProductCard: React.FC = () => {
+type Props = {
+  phone: Phone;
+};
+
+export const ProductCard: React.FC<Props> = ({ phone }) => {
+  // console.log(phone.image);
+  // console.log(`../../${phone.image}`);
+
   return (
-    <>
-      <article className={classes.card}>
-        <img className={classes.photo} src={img} alt="Phone card" />
+    <article className={classes.card}>
+      {/* handle issue with images
+      when passing {phone.image} it is not connecting img
+      even the path is the same as  */}
+      <img
+        className={classes.photo}
+        src={img}
+        alt="Phone card"
+      />
 
-        <h3 className={classes.header}>
-          Apple iPhone Xs 64GB Silver (iMT9G2FS/A)
-        </h3>
+      <h3 className={classes.header}>
+        {phone.name}
+      </h3>
 
-        <p className={classes.price}>
-          <span className={classes.actual}>$799</span>
-          <span className={classes.preliminary}>$899</span>
+      <p className={classes.price}>
+        <span className={classes.actual}>{phone.fullPrice}</span>
+        <span className={classes.preliminary}>{phone.price}</span>
+      </p>
+
+      <div className={classes.line} />
+
+      <div className={classes.information}>
+        <p className={classes.block}>
+          <span className={classes.text}>Screen</span>
+          <span className={classes.value}>{phone.screen}</span>
         </p>
 
-        <div className={classes.line} />
+        <p className={classes.block}>
+          <span className={classes.text}>Capacity</span>
+          <span className={classes.value}>{phone.capacity}</span>
+        </p>
 
-        <div className={classes.information}>
-          <p className={classes.block}>
-            <span className={classes.text}>Screen</span>
-            <span className={classes.value}>5.8” OLED</span>
-          </p>
+        <p className={classes.block}>
+          <span className={classes.text}>RAM</span>
+          <span className={classes.value}>{phone.ram}</span>
+        </p>
 
-          <p className={classes.block}>
-            <span className={classes.text}>Capacity</span>
-            <span className={classes.value}>64 GB</span>
-          </p>
-
-          <p className={classes.block}>
-            <span className={classes.text}>RAM</span>
-            <span className={classes.value}>4 GB</span>
-          </p>
-
-          <p className={classes.block}>
-            <PrimaryButton />
-            <IconButton />
-          </p>
-        </div>
-      </article>
-    </>
+        <p className={classes.block}>
+          {/* add props text on Default,
+          text on active
+          is active will be passed as a prop also
+          functions on change default
+          on change when active
+          create container for button to handle it width (80% from containe) */}
+          <PrimaryButton />
+          <IconButton />
+        </p>
+      </div>
+    </article>
   );
 };
