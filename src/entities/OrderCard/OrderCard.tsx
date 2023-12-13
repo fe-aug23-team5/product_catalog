773f0de39128ad5e5b135d722b20482290c22043
Dmytro <tilugdmytro@gmail.com>
2023-12-14 00:28:59 +0200

    add functionality

--- src/entities/OrderCard/OrderCard.tsx
+++ src/entities/OrderCard/OrderCard.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 /* eslint-disable max-len */
 import React, { useContext } from 'react';
 import { GlobalContext } from '../../shared/utils/GlobalProvider';
@@ -8,79 +9,120 @@ import plusIcon from '../../img/icons/plus.svg';
 import { PrimaryButton } from '../../shared/ui/PrimaryButton';
 
 export const OrderCard: React.FC = () => {
-  const { cart } = useContext(GlobalContext);
+  const { cart, deleteCartItem, updateCartItemQuantity } = useContext(GlobalContext);
+
+  const handleDecrease = (phoneId: string) => {
+    const selectedPhone = cart.find(phone => phone.phoneId === phoneId);
+
+    if (selectedPhone && selectedPhone.quantity > 1) {
+      updateCartItemQuantity(selectedPhone.phoneId, selectedPhone.quantity - 1);
+    }
+  };
+
+  const handleIncrease = (phoneId: string) => {
+    const selectedPhone = cart.find(phone => phone.phoneId === phoneId);
+
+    if (selectedPhone) {
+      updateCartItemQuantity(selectedPhone.phoneId, selectedPhone.quantity + 1);
+    }
+  };
+
+  const calculatePrice = (price: number, quantity: number): number => {
+    return price * quantity;
+  };
+
+  const calculateTotalPrice = (): number => {
+    return cart.reduce((total, phone) => total + calculatePrice(phone.price, phone.quantity), 0);
+  };
+
+  const calculateTotalItems = (): number => {
+    return cart.reduce((total, phone) => total + phone.quantity, 0);
+  };
 
   const baseUrl = 'https://raw.githubusercontent.com/fe-aug23-team5/product_catalog-static/main/';
 
-  // console.log(cart);
+  const handleDefaultAction = () => {
+    console.log('Default action');
+  };
 
   return (
     <div className={styles.cart__page_container}>
-      <ul>
-        {cart.map(({
-          id, name, image, price,
-        }) => (
-          <li key={id} className={styles.phone__card}>
-            <div className={styles.phone__card_container}>
-              <button className={styles.phone__card_delete_button} type="button">
+      {cart.length > 0 ? (
+        <ul>
+          {cart.map(({
+            id, name, image, price, phoneId, quantity,
+          }) => (
+            <li key={id} className={styles.phone__card}>
+              <div className={styles.phone__card_container}>
+                <button
+                  className={styles.phone__card_delete_button}
+                  type="button"
+                  onClick={() => deleteCartItem(phoneId)}
+                >
+                  <img
+                    src={closeIcon}
+                    alt="closeIcon"
+                  />
+                </button>
                 <img
-                  src={closeIcon}
-                  alt="closeIcon"
+                  className={styles.phone__card_image}
+                  src={`${baseUrl}${image}`}
+                  alt={name}
                 />
-              </button>
-              <img
-                className={styles.phone__card_image}
-                src={`${baseUrl}${image}`}
-                alt={name}
-              />
-              <h3 className={styles.phone__card_title}>{name}</h3>
-            </div>
-            <div className={styles.phone__card_quantity_container}>
-              <div className={styles.phone__card_button_container}>
-                <button
-                  type="button"
-                  className={styles.phone__card_quantity_button}
-                >
-                  <img
-                    src={minusIcon}
-                    alt="closeIcon"
-                  />
-                </button>
-                <p className={styles.phone__card_quantity}>
-                  1
-                </p>
-                <button
-                  type="button"
-                  className={styles.phone__card_quantity_button}
-                >
-                  <img
-                    src={plusIcon}
-                    alt="closeIcon"
-                  />
-                </button>
+                <h3 className={styles.phone__card_title}>{name}</h3>
               </div>
-              <p className={styles.phone__card_price}>{`$${price}`}</p>
-            </div>
-          </li>
-        ))}
-      </ul>
+              <div className={styles.phone__card_quantity_container}>
+                <div className={styles.phone__card_button_container}>
+                  <button
+                    type="button"
+                    className={styles.phone__card_quantity_button}
+                    onClick={() => handleDecrease(phoneId)}
+                  >
+                    <img
+                      src={minusIcon}
+                      alt="closeIcon"
+                    />
+                  </button>
+                  <p className={styles.phone__card_quantity}>
+                    {quantity}
+                  </p>
+                  <button
+                    type="button"
+                    className={styles.phone__card_quantity_button}
+                    onClick={() => handleIncrease(phoneId)}
+                  >
+                    <img
+                      src={plusIcon}
+                      alt="closeIcon"
+                    />
+                  </button>
+                </div>
+                <p className={styles.phone__card_price}>{`$${calculatePrice(price, quantity)}`}</p>
+              </div>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p className={styles.phone__card_message}>Your cart is empty</p>
+      )}
       <div className={styles.cart__page_amount}>
-        {/* <div className={styles.cart__page_total_amount}> */}
         <div className={styles.cart__page_price}>
           <p className={styles.cart__page_total_price}>
-            $2657
+            {`$${calculateTotalPrice()}`}
           </p>
 
           <p className={styles.cart__page_items_length}>
-            Total for 3 items
+            {`Total for ${calculateTotalItems()} items`}
           </p>
         </div>
 
-        <div className="cart-page__break-line" />
+        <div className={styles.cart__page_break_line} />
 
-        <PrimaryButton />
+        <PrimaryButton
+          defaultAction={handleDefaultAction}
+          defaultTitle="Checkout"
+        />
       </div>
-      {/* </div> */}
     </div>
   );
 };
