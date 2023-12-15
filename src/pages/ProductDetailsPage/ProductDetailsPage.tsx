084b515df947f150dd7683646ae8a9081a1dc576
Oleh Kuper <ok1804@ukr.net>
2023-12-15 09:13:55 +0200

    add layout page detail

--- src/pages/ProductDetailsPage/ProductDetailsPage.tsx
+++ src/pages/ProductDetailsPage/ProductDetailsPage.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/dot-notation */
 /* eslint-disable jsx-a11y/control-has-associated-label */
 import React, { useEffect, useState } from 'react';
 import { getPhoneById } from '../../shared/api/phones';
@@ -42,7 +43,9 @@ export const ProductDetailsPage: React.FC = () => {
             alt="Rigth arrow icon"
           />
 
-          <span className={styles.span}>{productDetail?.name}</span>
+          <span className={styles.span}>
+            {productDetail?.name}
+          </span>
         </div>
 
         <button className={styles.goback_button} type="button">
@@ -79,192 +82,221 @@ export const ProductDetailsPage: React.FC = () => {
               </li>
             ))}
           </ul>
-        </article>
 
-        <article className={styles.color}>
-          <div className={styles.color__text}>
-            <span className={styles.color__description}>
-              Available colors
-            </span>
+          <article className={styles.product__info}>
+            <div className={styles.color}>
+              <div className={styles.color__text}>
+                <span className={styles.color__description}>
+                  Available colors
+                </span>
 
-            <span className={styles.color__description}>
-              ID:123
-            </span>
-          </div>
+                <span className={styles.color__description}>
+                  ID:123
+                </span>
+              </div>
 
-          <ul className={styles.color__list}>
-            {productDetail?.colorsAvailable.map(color => (
-              <li key={color} className={styles.color__item}>
-                <button
-                  style={{ backgroundColor: `${color}` }}
-                  className={styles.color__button}
-                  type="button"
-                />
-              </li>
-            ))}
-          </ul>
-        </article>
+              <ul className={styles.color__list}>
+                {productDetail?.colorsAvailable.map(color => (
+                  <li key={color} className={styles.color__item}>
+                    <button
+                      style={{ backgroundColor: `${color}` }}
+                      className={styles.color__button}
+                      type="button"
+                    />
+                  </li>
+                ))}
+              </ul>
+            </div>
 
-        <article className={styles.capacity}>
-          <p className={styles.capacity__text}>
-            Select capacity
-          </p>
-
-          <ul className={styles.capacity__list}>
-            {productDetail?.capacityAvailable.map(capacity => (
-              <li key={capacity} className={styles.capacity__item}>
-                <button type="button" className={styles.capacity__button}>
-                  {capacity}
-                </button>
-              </li>
-            ))}
-          </ul>
-        </article>
-
-        <article className={styles.price}>
-          <p className={styles.price__amount}>
-            <span>{productDetail?.priceDiscount}</span>
-            <span>{productDetail?.priceRegular}</span>
-          </p>
-
-          <div className={styles.price__buttons}>
-            <PrimaryButton />
-            <IconButton />
-          </div>
-
-          <div className={styles.price__text_wrapper}>
-            <p className={styles.price__text}>
-              <span>Screen</span>
-              <span>{productDetail?.screen}</span>
-            </p>
-
-            <p className={styles.price__text}>
-              <span>Resolution</span>
-              <span>{productDetail?.resolution}</span>
-            </p>
-
-            <p className={styles.price__text}>
-              <span>Processor</span>
-              <span>{productDetail?.processor}</span>
-            </p>
-
-            <p className={styles.price__text}>
-              <span>RAM</span>
-              <span>{productDetail?.ram}</span>
-            </p>
-          </div>
-        </article>
-      </section>
-
-      <section className={styles.about}>
-        <h2 className={styles.about__title}>
-          About
-        </h2>
-
-        {productDetail?.description.map(info => {
-          const { text, title } = info;
-
-          return (
-            <article key={title} className={styles.description}>
-              <h3 className={styles.description__title}>
-                {title}
-              </h3>
-
-              <p className={styles.description__text}>
-                {text}
+            <div className={styles.capacity}>
+              <p className={styles.capacity__text}>
+                Select capacity
               </p>
-            </article>
-          );
-        })}
-      </section>
 
-      <section className={styles.tech}>
-        <h2 className={styles.tech__title}>
-          Tech specs
-        </h2>
+              <ul className={styles.capacity__list}>
+                {productDetail?.capacityAvailable.map(capacity => (
+                  <li key={capacity} className={styles.capacity__item}>
+                    <button type="button" className={styles.capacity__button}>
+                      {capacity}
+                    </button>
+                  </li>
+                ))}
+              </ul>
+            </div>
 
-        <article className={styles.tech__details}>
-          <p className={styles.tech__wrapper}>
-            <span className={styles.tech__key}>
-              Screen
-            </span>
+            <div className={styles.price}>
+              <p className={styles.price__amount}>
+                <span className={styles.price__discount}>
+                  {`$${productDetail?.priceDiscount}`}
+                </span>
 
-            <span className={styles.tech__value}>
-              {productDetail?.screen}
-            </span>
-          </p>
+                <span className={styles.price__regular}>
+                  {`$${productDetail?.priceRegular}`}
+                </span>
+              </p>
 
-          <p className={styles.tech__wrapper}>
-            <span className={styles.tech__key}>
-              Resolution
-            </span>
+              <div className={styles.price__buttons}>
+                <PrimaryButton />
+                <IconButton />
+              </div>
 
-            <span className={styles.tech__value}>
-              {productDetail?.resolution}
-            </span>
-          </p>
+              <div className={styles.details}>
+                <p className={styles.details__text}>
+                  <span className={styles.details__key}>
+                    Screen
+                  </span>
 
-          <p className={styles.tech__wrapper}>
-            <span className={styles.tech__key}>
-              Processor
-            </span>
+                  <span className={styles.details__value}>
+                    {productDetail?.screen}
+                  </span>
+                </p>
 
-            <span className={styles.tech__value}>
-              {productDetail?.processor}
-            </span>
-          </p>
+                <p className={styles.details__text}>
+                  <span className={styles.details__key}>
+                    Resolution
+                  </span>
 
-          <p className={styles.tech__wrapper}>
-            <span className={styles.tech__key}>
-              RAM
-            </span>
+                  <span className={styles.details__value}>
+                    {productDetail?.resolution}
+                  </span>
+                </p>
 
-            <span className={styles.tech__value}>
-              {productDetail?.ram}
-            </span>
-          </p>
+                <p className={styles.details__text}>
+                  <span className={styles.details__key}>
+                    Processor
+                  </span>
 
-          <p className={styles.tech__wrapper}>
-            <span className={styles.tech__key}>
-              Built in memory
-            </span>
+                  <span className={styles.details__value}>
+                    {productDetail?.processor}
+                  </span>
+                </p>
 
-            <span className={styles.tech__value}>
-              {productDetail?.capacity}
-            </span>
-          </p>
+                <p className={styles.details__text}>
+                  <span className={styles.details__key}>
+                    RAM
+                  </span>
 
-          <p className={styles.tech__wrapper}>
-            <span className={styles.tech__key}>
-              Camera
-            </span>
-
-            <span className={styles.tech__value}>
-              {productDetail?.camera}
-            </span>
-          </p>
-
-          <p className={styles.tech__wrapper}>
-            <span className={styles.tech__key}>
-              Zoom
-            </span>
-
-            <span className={styles.tech__value}>
-              {productDetail?.zoom}
-            </span>
-          </p>
-
-          <p className={styles.tech__wrapper}>
-            <span className={styles.tech__key}>
-              Cell
-            </span>
-
-            <span className={styles.tech__value}>
-              {productDetail?.cell.join(', ')}
-            </span>
-          </p>
+                  <span className={styles.details__value}>
+                    {productDetail?.ram}
+                  </span>
+                </p>
+              </div>
+            </div>
+          </article>
         </article>
       </section>
+
+      <div className={styles.product_details__section_wrapper}>
+        <section className={styles.about}>
+          <h2 className={styles.about__title}>
+            About
+          </h2>
+
+          {productDetail?.description.map(info => {
+            const { text, title } = info;
+
+            return (
+              <article key={title} className={styles.description}>
+                <h3 className={styles.description__title}>
+                  {title}
+                </h3>
+
+                <p className={styles.description__text}>
+                  {text}
+                </p>
+              </article>
+            );
+          })}
+        </section>
+
+        <section className={styles.tech}>
+          <h2 className={styles.tech__title}>
+            Tech specs
+          </h2>
+
+          <article className={styles.tech__details}>
+            <p className={styles.tech__wrapper}>
+              <span className={styles.tech__key}>
+                Screen
+              </span>
+
+              <span className={styles.tech__value}>
+                {productDetail?.screen}
+              </span>
+            </p>
+
+            <p className={styles.tech__wrapper}>
+              <span className={styles.tech__key}>
+                Resolution
+              </span>
+
+              <span className={styles.tech__value}>
+                {productDetail?.resolution}
+              </span>
+            </p>
+
+            <p className={styles.tech__wrapper}>
+              <span className={styles.tech__key}>
+                Processor
+              </span>
+
+              <span className={styles.tech__value}>
+                {productDetail?.processor}
+              </span>
+            </p>
+
+            <p className={styles.tech__wrapper}>
+              <span className={styles.tech__key}>
+                RAM
+              </span>
+
+              <span className={styles.tech__value}>
+                {productDetail?.ram}
+              </span>
+            </p>
+
+            <p className={styles.tech__wrapper}>
+              <span className={styles.tech__key}>
+                Built in memory
+              </span>
+
+              <span className={styles.tech__value}>
+                {productDetail?.capacity}
+              </span>
+            </p>
+
+            <p className={styles.tech__wrapper}>
+              <span className={styles.tech__key}>
+                Camera
+              </span>
+
+              <span className={styles.tech__value}>
+                {productDetail?.camera}
+              </span>
+            </p>
+
+            <p className={styles.tech__wrapper}>
+              <span className={styles.tech__key}>
+                Zoom
+              </span>
+
+              <span className={styles.tech__value}>
+                {productDetail?.zoom}
+              </span>
+            </p>
+
+            <p className={styles.tech__wrapper}>
+              <span className={styles.tech__key}>
+                Cell
+              </span>
+
+              <span className={styles.tech__value}>
+                {productDetail?.cell.join(', ')}
+              </span>
+            </p>
+          </article>
+        </section>
+      </div>
     </div>
   );
 };
