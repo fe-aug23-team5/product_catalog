27929384044c7c4a68f64f659ae46f1a3ccb5fc9
elenagrasovskaya <elenagrasovskaya@gmail.com>
2023-12-15 18:23:42 +0200

    Pagination search params logick and layout

--- src/pages/PhonesPage/PhonesPage.tsx
+++ src/pages/PhonesPage/PhonesPage.tsx
@@ -1,17 +1,27 @@
 import React, { useEffect, useState } from 'react';
 import './PhonesPage.scss';
+import { useSearchParams } from 'react-router-dom';
 import { Phone } from '../../shared/types/Phone';
-import { getAllPhones } from '../../shared/api/phones';
+import { getAllPhonesWithParams } from '../../shared/api/phones';
 import { ProductCard } from '../../entities/ProductCard';
+import { Pagination } from '../../features/Pagination';
 
 export const PhonesPage: React.FC = () => {
   const [allPhones, setAllPhones] = useState<Phone[]>([]);
+  const [totalCount, setTotalCount] = useState<number>(0);
+  const [searchParams, setSearchParams] = useState('');
+  const newSearchParams = useSearchParams();
+
+  if (newSearchParams.toString().split(',')[0] !== searchParams) {
+    setSearchParams(newSearchParams.toString().split(',')[0]);
+  }
 
   const fetchPhones = async () => {
     try {
-      const phones = await getAllPhones();
+      const phones = await getAllPhonesWithParams(searchParams);
 
       setAllPhones(phones.data);
+      setTotalCount(phones.totalCount);
     } catch (error) {
       throw new Error('Unexpected Error');
     }
@@ -19,11 +29,16 @@ export const PhonesPage: React.FC = () => {
 
   useEffect(() => {
     fetchPhones();
-  }, []);
+  }, [searchParams]);
 
   return (
-    <div className="phones_container">
-      {allPhones.map(phone => <ProductCard key={phone.id} phone={phone} />)}
+    <div className="phones">
+      <div className="phones_container">
+        {allPhones.map((phone) => (
+          <ProductCard key={phone.id} phone={phone} />
+        ))}
+      </div>
+      {allPhones.length > 0 && <Pagination totalCount={totalCount} />}
     </div>
   );
 };
--- src/shared/api/phones.ts
+++ src/shared/api/phones.ts
@@ -3,6 +3,7 @@ import { getData } from '../helpers/fetchClient';
 import { ServerResponce } from '../types/ServerResponse';
 
 export const getAllPhones = () => getData<ServerResponce>('/phones');
+export const getAllPhonesWithParams = (searchParams:string) => getData<ServerResponce>(`/phones?${searchParams}`);
 // to access detailed phone data use phoneId object field
 // (smth like that apple-iphone-7-32gb-black)
 export const getPhoneById = (phoneId: string) => getData<Phone>(`/phones/${phoneId}`);
