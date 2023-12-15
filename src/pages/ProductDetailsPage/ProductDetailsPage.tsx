7e0d463f6bd3af10600e968398bac07a91fe7f96
Oleh Kuper <ok1804@ukr.net>
2023-12-15 18:26:53 +0200

    add functioanlity

--- src/pages/ProductDetailsPage/ProductDetailsPage.tsx
+++ src/pages/ProductDetailsPage/ProductDetailsPage.tsx
@@ -1,47 +1,43 @@
 /* eslint-disable no-console */
-/* eslint-disable @typescript-eslint/dot-notation */
 /* eslint-disable jsx-a11y/control-has-associated-label */
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import cn from 'classnames';
 import { getPhoneById } from '../../shared/api/phones';
 import { PhoneDetails } from '../../shared/types/PhoneDetails';
 import styles from './ProductDetails.module.scss';
-import homeIcon from '../../img/icons/Home_icon.svg';
-import rightArrow from '../../img/icons/right_arrow.svg';
 import leftArrow from '../../img/icons/Chevron (Arrow Right).svg';
 import { BASE_URL_IMG } from '../../shared/helpers/fetchClient';
+import { PrimaryButton } from '../../shared/ui/PrimaryButton';
+import { IconButton } from '../../shared/ui/IconButton';
+import { Breadcrumbs } from '../../features/Breadcrumbs';
 import { Loader } from '../../widgets/Loader';
-// import { GlobalContext } from '../../shared/utils/GlobalProvider';
-// import { PrimaryButton } from '../../shared/ui/PrimaryButton';
-// import { IconButton } from '../../shared/ui/IconButton';
 
 export const ProductDetailsPage: React.FC = () => {
   const location = useLocation();
   const [productDetail, setProductDetail] = useState<PhoneDetails | null>(null);
+  const [productImage, setProductImage] = useState('');
   const [isLoad, setIsLoad] = useState(true);
+  const [capacity, setCapacity] = useState(productDetail?.capacity);
+
+  const productId = useRef(location.pathname
+    .split('/')[2].split('-').slice(0, -1).join('-'));
+
   const [
     productColor, setProductColor,
   ] = useState(location.pathname.split('-').at(-1));
 
-  const imgUrl = `${BASE_URL_IMG}${productDetail?.images[0]}`;
-  const productId = location.pathname
-    .split('/')[2].split('-').slice(0, -1).join('-');
-
   useEffect(() => {
-    getPhoneById(`${productId}-${productColor}`)
-      .then((data) => setProductDetail(data))
+    getPhoneById(`${productId.current}-${productColor}`)
+      .then((data) => {
+        setProductDetail(data);
+        setProductImage(`${BASE_URL_IMG}${data.images[0]}`);
+      })
       .catch(error => {
         throw error;
       })
       .finally(() => setIsLoad(false));
-  }, [productId, productColor]);
-
-  // const changeProductColor = useCallback((color: string) => {
-  //   setIsLoad(true);
-  //   setProductColor(color);
-  //   console.log(color, productColor);
-  // }, [productColor]);
+  }, [capacity, productColor]);
 
   const changeProductColor = (color: string) => {
     if (color === productColor) {
@@ -50,46 +46,25 @@ export const ProductDetailsPage: React.FC = () => {
 
     setIsLoad(true);
     setProductColor(color);
-
-    console.log(color, productColor);
   };
 
-  // const {
-  //   cart,
-  //   addCartItem,
-  //   deleteCartItem,
-  //   favourites,
-  //   addFavouriteItem,
-  //   deleteFavouriteItem,
-  // } = useContext(GlobalContext);
+  const changeCapacity = (value: string) => {
+    if (value === capacity) {
+      return;
+    }
+
+    productId.current = productId.current
+      .split('-').map(item => (item.includes('gb') ? value : item)).join('-');
+    setIsLoad(true);
+    setCapacity(value);
+  };
 
   return isLoad
     ? (<Loader />)
     : (
       <div className={styles.product_details}>
-        <div className={styles.product_details__top}>
-          <div className={styles.product_details__path}>
-            <img className={styles.icon} src={homeIcon} alt="Home icon" />
-
-            <img
-              className={styles.icon}
-              src={rightArrow}
-              alt="Rigth arrow icon"
-            />
-
-            <span className={styles.span}>Phones</span>
-
-            <img
-              className={styles.icon}
-              src={rightArrow}
-              alt="Rigth arrow icon"
-            />
-
-            <span className={styles.span}>
-              {productDetail?.name}
-            </span>
-          </div>
-
+        <div className={styles.block_top}>
+          <Breadcrumbs />
           <button className={styles.goback_button} type="button">
             <img
               className={styles.icon}
@@ -98,148 +73,171 @@ export const ProductDetailsPage: React.FC = () => {
             />
             Back
           </button>
+
+          <h1 className={styles.section_image__title}>
+            {productDetail?.name}
+          </h1>
         </div>
 
-        <section className={styles.section_image}>
-          <h1 className={styles.section_image__title}>{productDetail?.name}</h1>
+        <div className={styles.block_gallery}>
+          <div className={styles.product__image_wrapper}>
+            <img
+              className={styles.product_main_image}
+              src={productImage}
+              alt="Product"
+            />
+          </div>
 
-          <article className={styles.product}>
-            <div className={styles.product__image_wrapper}>
-              <img
-                className={styles.product_main_image}
-                src={imgUrl}
-                alt="Product"
-              />
+          <ul className={styles.product__list}>
+            {productDetail?.images.map(image => (
+              <li key={image} className={styles.product__item}>
+                <button
+                  className={styles.product__button}
+                  type="button"
+                  onClick={() => setProductImage(`${BASE_URL_IMG}${image}`)}
+                >
+                  <img
+                    className={styles.product__image}
+                    src={`${BASE_URL_IMG}${image}`}
+                    alt="Product"
+                  />
+                </button>
+              </li>
+            ))}
+          </ul>
+        </div>
+
+        <div className={styles.block_info}>
+          <article className={styles.product__info}>
+            <div className={styles.color}>
+              <div className={styles.color__text}>
+                <span className={styles.color__description}>
+                  Available colors
+                </span>
+
+                <span className={styles.color__description}>
+                  ID:123
+                </span>
+              </div>
+
+              <ul className={styles.color__list}>
+                {productDetail?.colorsAvailable.map(color => (
+                  <li
+                    key={color}
+                    className={cn(styles.color__item, {
+                      [styles.color__item_active]: color === productColor,
+                    })}
+                  >
+                    <button
+                      onClick={() => changeProductColor(color)}
+                      style={{ backgroundColor: `${color}` }}
+                      className={styles.color__button}
+                      type="button"
+                    />
+                  </li>
+                ))}
+              </ul>
             </div>
 
-            <ul className={styles.product__list}>
-              {productDetail?.images.map(image => (
-                <li key={image} className={styles.product__item}>
-                  <button
-                    className={styles.product__button}
-                    type="button"
+            <div className={styles.capacity}>
+              <p className={styles.capacity__text}>
+                Select capacity
+              </p>
+
+              <ul className={styles.capacity__list}>
+                {productDetail?.capacityAvailable.map(capacityItem => (
+                  <li
+                    key={capacityItem}
+                    className={cn(styles.capacity__item, {
+                      [
+                      styles.capacity__item_active
+                      ]: capacityItem === productDetail.capacity,
+                    })}
                   >
-                    <img
-                      className={styles.product__image}
-                      src={`${BASE_URL_IMG}${image}`}
-                      alt="Product"
-                    />
-                  </button>
-                </li>
-              ))}
-            </ul>
-
-            <article className={styles.product__info}>
-              <div className={styles.color}>
-                <div className={styles.color__text}>
-                  <span className={styles.color__description}>
-                    Available colors
-                  </span>
-
-                  <span className={styles.color__description}>
-                    ID:123
-                  </span>
-                </div>
-
-                <ul className={styles.color__list}>
-                  {productDetail?.colorsAvailable.map(color => (
-                    <li
-                      key={color}
-                      // className={styles.color__item}
-                      className={cn(styles.color__item, {
-                        [styles.color__item_active]: color === productColor,
+                    <button
+                      type="button"
+                      className={cn(styles.capacity__button, {
+                        [
+                        styles.capacity__button_active
+                        ]: capacityItem === productDetail.capacity,
                       })}
+                      onClick={() => changeCapacity(capacityItem.toLowerCase())}
                     >
-                      <button
-                        onClick={() => changeProductColor(color)}
-                        style={{ backgroundColor: `${color}` }}
-                        className={styles.color__button}
-                        type="button"
-                      />
-                    </li>
-                  ))}
-                </ul>
+                      {capacityItem}
+                    </button>
+                  </li>
+                ))}
+              </ul>
+            </div>
+
+            <div className={styles.price}>
+              <p className={styles.price__amount}>
+                <span className={styles.price__discount}>
+                  {`$${productDetail?.priceDiscount}`}
+                </span>
+
+                <span className={styles.price__regular}>
+                  {`$${productDetail?.priceRegular}`}
+                </span>
+              </p>
+
+              <div className={styles.price__buttons}>
+                <PrimaryButton
+                  defaultAction={() => { }}
+                  defaultTitle="Checkout"
+                />
+
+                <IconButton
+                  defaultAction={() => { }}
+                />
               </div>
 
-              <div className={styles.capacity}>
-                <p className={styles.capacity__text}>
-                  Select capacity
-                </p>
-
-                <ul className={styles.capacity__list}>
-                  {productDetail?.capacityAvailable.map(capacity => (
-                    <li key={capacity} className={styles.capacity__item}>
-                      <button type="button" className={styles.capacity__button}>
-                        {capacity}
-                      </button>
-                    </li>
-                  ))}
-                </ul>
-              </div>
-
-              <div className={styles.price}>
-                <p className={styles.price__amount}>
-                  <span className={styles.price__discount}>
-                    {`$${productDetail?.priceDiscount}`}
+              <div className={styles.details}>
+                <p className={styles.details__text}>
+                  <span className={styles.details__key}>
+                    Screen
                   </span>
 
-                  <span className={styles.price__regular}>
-                    {`$${productDetail?.priceRegular}`}
+                  <span className={styles.details__value}>
+                    {productDetail?.screen}
                   </span>
                 </p>
 
-                <div className={styles.price__buttons}>
-                  {/* <PrimaryButton />
-                  <IconButton /> */}
-                </div>
+                <p className={styles.details__text}>
+                  <span className={styles.details__key}>
+                    Resolution
+                  </span>
 
-                <div className={styles.details}>
-                  <p className={styles.details__text}>
-                    <span className={styles.details__key}>
-                      Screen
-                    </span>
+                  <span className={styles.details__value}>
+                    {productDetail?.resolution}
+                  </span>
+                </p>
 
-                    <span className={styles.details__value}>
-                      {productDetail?.screen}
-                    </span>
-                  </p>
+                <p className={styles.details__text}>
+                  <span className={styles.details__key}>
+                    Processor
+                  </span>
 
-                  <p className={styles.details__text}>
-                    <span className={styles.details__key}>
-                      Resolution
-                    </span>
+                  <span className={styles.details__value}>
+                    {productDetail?.processor}
+                  </span>
+                </p>
 
-                    <span className={styles.details__value}>
-                      {productDetail?.resolution}
-                    </span>
-                  </p>
+                <p className={styles.details__text}>
+                  <span className={styles.details__key}>
+                    RAM
+                  </span>
 
-                  <p className={styles.details__text}>
-                    <span className={styles.details__key}>
-                      Processor
-                    </span>
-
-                    <span className={styles.details__value}>
-                      {productDetail?.processor}
-                    </span>
-                  </p>
-
-                  <p className={styles.details__text}>
-                    <span className={styles.details__key}>
-                      RAM
-                    </span>
-
-                    <span className={styles.details__value}>
-                      {productDetail?.ram}
-                    </span>
-                  </p>
-                </div>
+                  <span className={styles.details__value}>
+                    {productDetail?.ram}
+                  </span>
+                </p>
               </div>
-            </article>
+            </div>
           </article>
-        </section>
+        </div>
 
-        <div className={styles.product_details__section_wrapper}>
+        <div className={styles.block_about}>
           <section className={styles.about}>
             <h2 className={styles.about__title}>
               About
@@ -261,7 +259,9 @@ export const ProductDetailsPage: React.FC = () => {
               );
             })}
           </section>
+        </div>
 
+        <div className={styles.block_tech}>
           <section className={styles.tech}>
             <h2 className={styles.tech__title}>
               Tech specs
