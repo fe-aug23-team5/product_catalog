ccd48c677195d5824d1d22570b16dac2ee1cfc4d
Yuliia Bakun <bakunyuliia@gmail.com>
2023-12-15 15:45:42 +0100

    catalog grid and small fixes

--- src/pages/PhonesPage/PhonesPage.tsx
+++ src/pages/PhonesPage/PhonesPage.tsx
@@ -4,9 +4,11 @@ import { Phone } from '../../shared/types/Phone';
 import { getAllPhones } from '../../shared/api/phones';
 import { ProductCard } from '../../entities/ProductCard';
 import { Dropdown } from '../../shared/ui/Dropdown';
+import { Loader } from '../../widgets/Loader';
 
 export const PhonesPage: React.FC = () => {
   const [allPhones, setAllPhones] = useState<Phone[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   const filteredOptions = [
     { label: 'Newest', value: 'newest' },
@@ -22,12 +24,16 @@ export const PhonesPage: React.FC = () => {
   ];
 
   const fetchPhones = async () => {
+    setIsLoading(true);
+
     try {
       const phones = await getAllPhones();
 
       setAllPhones(phones.data);
     } catch (error) {
       throw new Error('Unexpected Error');
+    } finally {
+      setIsLoading(false);
     }
   };
 
@@ -53,7 +59,16 @@ export const PhonesPage: React.FC = () => {
       </div>
 
       <div className="phones__container">
-        {allPhones.map(phone => <ProductCard key={phone.id} phone={phone} />)}
+
+        {isLoading && <Loader />}
+
+        {!isLoading && allPhones.length && (
+          allPhones.map(phone => <ProductCard key={phone.id} phone={phone} />)
+        )}
+
+        {/* {!isLoading && !allPhones.length && (
+          <ErrorMessage />
+        )} */}
       </div>
     </div>
   );
