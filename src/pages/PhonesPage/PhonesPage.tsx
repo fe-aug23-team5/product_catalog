6481b09350b0b075e8939e7d2ed10aad3a23aca0
Yuliia Bakun <bakunyuliia@gmail.com>
2023-12-17 17:30:43 +0100

    mobile phones page logic

--- src/pages/PhonesPage/PhonesPage.tsx
+++ src/pages/PhonesPage/PhonesPage.tsx
@@ -8,36 +8,29 @@ import { Dropdown } from '../../shared/ui/Dropdown';
 import { Loader } from '../../widgets/Loader';
 import { Pagination } from '../../features/Pagination';
 import { Breadcrumbs } from '../../features/Breadcrumbs';
+import { sortOptions, itemsOnPage } from '../../shared/helpers/searchParams';
+import { getSearchWith } from '../../shared/helpers/searchHelper';
 
 export const PhonesPage: React.FC = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const sortBy = searchParams.get('sortBy') || 'name';
+  const perPage = searchParams.get('perPage') || '16';
+  const page = searchParams.get('page');
+
+  const [isLoading, setIsLoading] = useState(false);
   const [allPhones, setAllPhones] = useState<Phone[]>([]);
   const [totalCount, setTotalCount] = useState<number>(0);
-  const [searchParams, setSearchParams] = useState('');
-  const newSearchParams = useSearchParams();
-  const [isLoading, setIsLoading] = useState(false);
 
-  if (newSearchParams.toString().split(',')[0] !== searchParams) {
-    setSearchParams(newSearchParams.toString().split(',')[0]);
-  }
-
-  const filteredOptions = [
-    { label: 'Newest', value: 'newest' },
-    { label: 'Oldest', value: 'oldest' },
-    { label: 'PriceLowest', value: 'priceLowest' },
-    { label: 'PriceHighest', value: 'priceHighest' },
-  ];
-
-  const itemsOnPage = [
-    { label: '4', value: 4 },
-    { label: '8', value: 8 },
-    { label: '16', value: 16 },
-  ];
+  // const newSearchParams = useSearchParams();
+  // if (newSearchParams.toString().split(',')[0] !== searchParams) {
+  //   setSearchParams(newSearchParams.toString().split(',')[0]);
+  // }
 
   const fetchPhones = async () => {
     setIsLoading(true);
 
     try {
-      const phones = await getAllPhonesWithParams(searchParams);
+      const phones = await getAllPhonesWithParams(searchParams.toString());
 
       setAllPhones(phones.data);
       setTotalCount(phones.totalCount);
@@ -52,6 +45,24 @@ export const PhonesPage: React.FC = () => {
     fetchPhones();
     // eslint-disable-next-line
   }, [searchParams]);
+
+  useEffect(() => {
+    const visibleSearchParams = {
+      sortBy: sortBy === 'name' ? null : sortBy,
+      perPage: perPage === '16' ? null : perPage,
+      page: page === '1' ? null : page,
+    };
+
+    setSearchParams(getSearchWith(visibleSearchParams, searchParams));
+  }, [sortBy, perPage, page, searchParams, setSearchParams]);
+
+  const handleSortByChange = (value: string) => {
+    setSearchParams(getSearchWith({ sortBy: value }, searchParams));
+  };
+
+  const handlePerPageChange = (value: string) => {
+    setSearchParams(getSearchWith({ perPage: value }, searchParams));
+  };
 
   return (
     <div className="phonesPage">
@@ -69,13 +80,21 @@ export const PhonesPage: React.FC = () => {
             <p className="dropdown__title">
               Sort by
             </p>
-            <Dropdown options={filteredOptions} />
+            <Dropdown
+              options={sortOptions}
+              value={sortBy}
+              onChange={handleSortByChange}
+            />
           </div>
           <div className="dropdown__items">
             <p className="dropdown__title">
               Items on page
             </p>
-            <Dropdown options={itemsOnPage} />
+            <Dropdown
+              options={itemsOnPage}
+              value={perPage}
+              onChange={handlePerPageChange}
+            />
           </div>
         </div>
       </div>
