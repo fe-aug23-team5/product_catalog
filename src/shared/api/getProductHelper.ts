9db99c3ae087a8a5adc912a73e77c0acd9fa75a2
Yuliia Bakun <bakunyuliia@gmail.com>
2023-12-19 14:49:00 +0100

    added fetch helper and fixed types

--- src/shared/api/getProductHelper.ts
+++ src/shared/api/getProductHelper.ts
@@ -1,8 +1,9 @@
+/* eslint-disable max-len */
 import { getData } from '../helpers/fetchClient';
 import { Product } from '../types/Product';
-import { getAccById } from './accessories';
-import { getPhoneById } from './phones';
-import { getTabletById } from './tablets';
+import { getAccById, getAccDetailsById } from './accessories';
+import { getPhoneById, getPhoneDetailsById } from './phones';
+import { getTabletById, getTabletDetailsById } from './tablets';
 
 export const getProductById = (productType: string, productId: string) => {
   switch (productType) {
@@ -14,6 +15,19 @@ export const getProductById = (productType: string, productId: string) => {
       return getAccById(productId);
     default:
       return getPhoneById(productId);
+  }
+};
+
+export const getProductDetailsById = (productType: string, productId: string) => {
+  switch (productType) {
+    case 'iphone':
+      return getPhoneDetailsById(productId);
+    case 'ipad':
+      return getTabletDetailsById(productId);
+    case 'watch':
+      return getAccDetailsById(productId);
+    default:
+      return getPhoneDetailsById(productId);
   }
 };
 
